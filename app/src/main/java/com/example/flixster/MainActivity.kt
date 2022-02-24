79c9b80dbfe804ac6563ffd997ef00b01ba2cd59
unknown <dueonkim89@Yahoo.com>
2022-02-24 14:07:47 -0800

    created DTO object, implement DTO in MainActivity

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.flixster
 
 import android.content.ContentValues.TAG
-import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
 
 private const val URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
-
 private const val TAG = "MainActivity"
 
-
 class MainActivity : AppCompatActivity() {
+    private val movies = mutableListOf<Movie>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,7 +32,13 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                 Log.i(TAG, "onSuccess: JSON data $json")
-                val movieJsonArray = json.jsonObject.getJSONArray("results")
+                try {
+                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                    Log.i(TAG, "Movie List $movies")
+                } catch (e: JSONException) {
+                    Log.e(TAG, "Encountered exception $e")
+                }
             }
 
         })
